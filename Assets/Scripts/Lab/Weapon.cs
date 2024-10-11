693d58eb3b3e3a1785d25b80f439b50dec20cd96
Dezxain <obbab31@gmail.com>
2024-10-11 17:10:45 +0700

    code

--- Assets/Scripts/Lab/Weapon.cs
+++ Assets/Scripts/Lab/Weapon.cs
@@ -6,6 +6,20 @@ public abstract class Weapon : MonoBehaviour
     public string owner;
     public abstract void Move();
     public abstract void OnHitWith(Character character);
+    private int damage;
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+        set
+        {
+            damage = value;
+        }
+    }
+    public string Owner { get; set; }
+
     public int GetShootDirection()
     {
         return 1; 
